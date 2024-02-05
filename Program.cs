df35383033d267636f38c89805e77df5f325179b
Bawnorton <bawnorton@me.com>
2024-02-05 15:57:52 -0800

    allow debugging in prod

--- Program.cs
+++ Program.cs
@@ -17,7 +17,11 @@ builder.Services.AddDbContextFactory<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
