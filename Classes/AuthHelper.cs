a3a4eec62193a9964ef1676b6782cbb02b5ba25a
Bawnorton <bawnorton@me.com>
2024-03-03 18:15:32 -0800

    * Delegate friends to friend component
    * Create auth helper for getting a Self reference and other Auth related queries
    
    * Added Unary to reduce overhead for func<T,T> shapes

---
+++ Classes/AuthHelper.cs
@@ -0,0 +1,75 @@
+using System.Security.Principal;
+using Bamboozlers.Classes.AppDbContext;
+using Bamboozlers.Classes.Func;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bamboozlers.Classes;
+
+public static class AuthHelper
+{
+    private static AuthenticationStateProvider _authStateProvider;
+    private static IDbContextFactory<AppDbContext.AppDbContext> _db;
+    private static User? _self;
+    private static IIdentity? _identity;
+    
+    public static void Init(AuthenticationStateProvider authStateProvider, IDbContextFactory<AppDbContext.AppDbContext> db)
+    {
+        _authStateProvider = authStateProvider;
+        _db = db;
+    } 
+    
+    /// <returns>
+    /// The current user.
+    /// </returns>
+    /// <param name="inculsionCallback">
+    /// What data to include in the query/>.
+    /// </param>
+    /// <exception cref="Exception">
+    /// If <see cref="IsAuthenticated"/> returns false.
+    /// </exception>
+    public static async Task<User> GetSelf(Unary<IQueryable<User>>? inculsionCallback)
+    {
+        if (_self is not null) return _self;
+        
+        var identity = GetIdentity();
+        if (identity is { IsAuthenticated: true })
+        {
+            await using var db = await _db.CreateDbContextAsync();
+            var query = db.Users.AsQueryable();
+            query = inculsionCallback?.Invoke(query) ?? query;
+            _self = await query.FirstAsync(u => u.UserName == identity.Name);
+        }
+        else
+        {
+            throw new Exception("User is not authenticated");
+        }
+
+        return _self;
+    }
+
+    /// <summary>
+    /// See <see cref="GetSelf(System.Func{System.Linq.IQueryable{Bamboozlers.Classes.AppDbContext.User},System.Linq.IQueryable{Bamboozlers.Classes.AppDbContext.User}}?)"/>.
+    /// </summary>
+    public static async Task<User> GetSelf()
+    {
+        return await GetSelf(null);
+    }
+
+    public static bool IsAuthenticated()
+    {
+        return GetIdentity() is { IsAuthenticated: true };
+    }
+    
+    /// <returns>
+    /// The identity of the current user, or null if the user is not authenticated.
+    /// </returns>
+    private static IIdentity? GetIdentity()
+    {
+        if (_identity is not null) return _identity;
+        
+        var authState = _authStateProvider.GetAuthenticationStateAsync().Result;
+        _identity = authState.User.Identity;
+        return _identity;
+    }
+}
---
+++ Classes/Func/Unary.cs
@@ -0,0 +1,6 @@
+namespace Bamboozlers.Classes.Func;
+
+/// <summary>
+/// Function that takes a single argument and returns a value of the same type.
+/// </summary>
+public delegate T Unary<T>(T value);
