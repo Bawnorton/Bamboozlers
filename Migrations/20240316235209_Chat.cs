8709dd2ab8306831089b5f03c14596c0d8c43ec4
Bawnorton <bawnorton@me.com>
2024-03-16 16:54:35 -0700

    Probably fix migrations

---
+++ Migrations/20240316235209_Chat.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bamboozlers.Migrations
+{
+    /// <inheritdoc />
+    public partial class Chat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
