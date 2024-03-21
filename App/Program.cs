cfa5e6da35fff2deb8eb4826062256a8c2e44b19
Bawnorton <bawnorton@me.com>
2024-03-21 13:56:13 -0700

    Now why isn't this documented anywhere?

--- App/Program.cs
+++ App/Program.cs
@@ -82,3 +82,5 @@ app.MapRazorComponents<App>()
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
 app.Run();
+
+public partial class Program;
--- Tests/Playwright/ChatJsTests.cs
+++ Tests/Playwright/ChatJsTests.cs
@@ -1,11 +1,26 @@
 using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
-using Assert = NUnit.Framework.Assert;
+using Tests.Playwright.Infrastructure;
 
 namespace Tests.Playwright;
 
 [Parallelizable(ParallelScope.Self)]
-public class ChatJsTests : BlazorJsTestBase
+public class ChatJsTests(CustomWebApplicationFactory fixture) : PageTest, IClassFixture<CustomWebApplicationFactory>
 {
-    
+    private readonly string _serverAddress = fixture.ServerAddress;
+
+    [Fact]
+    public async Task ChatJsTest()
+    {
+        using var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        var page = await browser.NewPageAsync();
+        
+        await page.GotoAsync(_serverAddress);
+        await Task.Delay(10000000);
+    }
 }
