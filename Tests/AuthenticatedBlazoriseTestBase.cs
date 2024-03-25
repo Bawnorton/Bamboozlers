f590c91c68298d2d6758a194df41967e9da6afa0
Bawnorton <bawnorton@me.com>
2024-03-25 16:26:04 -0700

    handle merge conflict

--- Tests/AuthenticatedBlazoriseTestBase.cs
+++ Tests/AuthenticatedBlazoriseTestBase.cs
@@ -1,26 +1,13 @@
 using Bamboozlers.Classes.AppDbContext;
-<<<<<<< HEAD
 using Bamboozlers.Classes.Networking;
-=======
-using Bamboozlers.Classes.Interop;
->>>>>>> main
-using Bamboozlers.Classes.Services;
 using Bamboozlers.Classes.Services.Authentication;
-using Blazorise.Modules;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.JSInterop;
 using Tests.Provider;
 
 namespace Tests;
 
 public class AuthenticatedBlazoriseTestBase : BlazoriseTestBase
 {
-<<<<<<< HEAD
-    protected readonly MockDatabaseProvider MockDatabaseProvider;
-    protected readonly MockAuthenticationProvider MockAuthenticationProvider;
-
-    protected readonly User Self;
-=======
     protected MockDatabaseProvider MockDatabaseProvider;
     protected MockAuthenticationProvider MockAuthenticationProvider;
     protected MockJsRuntimeProvider MockJsRuntimeProvider;
@@ -31,26 +18,20 @@ public class AuthenticatedBlazoriseTestBase : BlazoriseTestBase
     protected UserService UserService;
     
     protected User? Self;
->>>>>>> main
 
     protected AuthenticatedBlazoriseTestBase()
     {
         MockDatabaseProvider = new MockDatabaseProvider(Ctx);
-<<<<<<< HEAD
         Self = MockDatabaseProvider.GetDbContextFactory().CreateDbContext().Users.First();
-        MockAuthenticationProvider = new MockAuthenticationProvider(Ctx, Self);
-        
-        AuthHelper.Init(MockAuthenticationProvider.GetAuthStateProvider(), MockDatabaseProvider.GetDbContextFactory());
-        WebSocketHandler.Init();
-=======
         MockAuthenticationProvider = new MockAuthenticationProvider(Ctx);
         MockJsRuntimeProvider = new MockJsRuntimeProvider(Ctx);
         MockWebSocketServiceProvider = new MockWebSocketServiceProvider(Ctx);
         MockUserManager = new MockUserManager(Ctx, MockDatabaseProvider);
         
+        WebSocketHandler.Init();
+        
         AuthService = new AuthService(MockAuthenticationProvider.GetAuthStateProvider(),MockDatabaseProvider.GetDbContextFactory());
         UserService = new UserService(AuthService, MockUserManager.GetUserManager());
-        
         Ctx.Services.AddSingleton<IUserService>(UserService);
         Ctx.Services.AddSingleton<IAuthService>(AuthService);
     }
@@ -60,6 +41,5 @@ public class AuthenticatedBlazoriseTestBase : BlazoriseTestBase
         Self = user;
         await MockAuthenticationProvider.SetUser(user);
         UserService.Invalidate();
->>>>>>> main
     }
 }
