f6be9315c32b030801716b14485d0b838a86c8d6
bhunt02 <bridgette.hunt02@outlook.com>
2024-03-31 17:13:40 -0700

    more changes to tests it's getting insane at this point

--- Tests/GroupChatTests.cs
+++ Tests/GroupChatTests.cs
@@ -5,6 +5,8 @@ using Blazorise;
 using Blazorise.Modules;
 using Bunit.Extensions.WaitForHelpers;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -112,7 +114,8 @@ public class GroupChatTests : AuthenticatedBlazoriseTestBase
         );
     }
     
-    private async Task<bool> CompAddMember_CheckInviteListEntry(User self,
+    private async Task<bool> CompAddMember_CheckInviteListEntry(
+        User self,
         User friend, 
         Chat chat,
         IEnumerable<GroupInvite> invites,
@@ -160,12 +163,11 @@ public class GroupChatTests : AuthenticatedBlazoriseTestBase
         
         var component = Ctx.RenderComponent<CompAddMember>(
             parameters 
-                => parameters.Add(p => p.ChatID, subjectGroup.ID)
+                => parameters.Add(p => p.ChatID, 1)
         );
         
         // Assert: Check observed the observed Chats for component
         Assert.Equal(subjectGroup.ID, component.Instance.WatchedIDs[0]);
-        component.Render();
         
         // Assert: Check that the proper users are being displayed (friends) with proper options
         var friends = testFriendships.Where(f => f.User1ID == subjectUser.Id || f.User2ID == subjectUser.Id)
@@ -183,7 +185,7 @@ public class GroupChatTests : AuthenticatedBlazoriseTestBase
         
         component = Ctx.RenderComponent<CompAddMember>(
             parameters 
-                => parameters.Add(p => p.ChatID, subjectGroup.ID)
+                => parameters.Add(p => p.ChatID, 1)
         );
         
         // Assert: Assert that the details are no longer visible
@@ -206,36 +208,54 @@ public class GroupChatTests : AuthenticatedBlazoriseTestBase
         { 
             Assert.False(await CompAddMember_CheckInviteListEntry(subjectUser, friend, subjectGroup, testInvites, component));
         }
-        
-        // Arrange & Act: Test Observer Pattern
-        subjectUser = testUsers[8];
-        subjectGroup = testGroups[1];
-        
-        await SetUser(subjectUser);
-        UserService.Invalidate();
-        
-        component.SetParametersAndRender(
-            parameters 
-                => parameters.Add(p => p.ChatID, subjectGroup.ID)
-        );
-        
-        friends = testFriendships.Where(f => f.User1ID == subjectUser.Id || f.User2ID == subjectUser.Id)
-            .Select(s => s.User1ID == subjectUser.Id ? s.User2 : s.User1)
-            .ToList();
-        
-        output.WriteLine(component.Markup);
-        foreach (var friend in friends)
-        {
-            Assert.True(await CompAddMember_CheckInviteListEntry(subjectUser, friend, subjectGroup, testInvites, component));
-        }
-
-        var target = subjectGroup.Users.First(u => u.Id != subjectUser.Id);
-        await UserGroupService.RemoveGroupMember(subjectGroup.ID, target.Id);
-        
-        subjectGroup.Users.Remove(subjectGroup.Users.First(u => u.Id != subjectUser.Id));
-        
     }
 
+    private void CompChatSettings_CheckMemberDisplay(
+        User subjectUser, 
+        GroupChat subjectGroup, 
+        IRenderedFragment fragment)
+    {
+        foreach (var member in subjectGroup.Users)
+        {
+            var isMod = subjectGroup.Moderators.FirstOrDefault(u => u.Id == member.Id) is not null;
+            var memberDiv = fragment.Find($"#{member.UserName}_section");
+            if (isMod ||
+                member.Id == subjectGroup.OwnerID)
+            {
+                var badge = fragment.Find($"#{member.UserName}_badge");
+                Assert.Contains(
+                    member.Id == subjectGroup.OwnerID 
+                        ? "OWNER" 
+                        : "MODERATOR", 
+                    badge.TextContent
+                );
+            }
+
+            if (subjectUser.Id == subjectGroup.OwnerID)
+            {
+                if (member.Id != subjectUser.Id)
+                {
+                    var kickButton = fragment.Find($"#{member.UserName}_kickButton");
+                    var permsButton = fragment.Find($"#{member.UserName}_permsButton");
+                    if (isMod)
+                    {
+                        Assert.Contains("Revoke permissions", permsButton.TextContent);
+                    }
+                    else
+                    {
+                        Assert.Contains("Assign permissions", permsButton.TextContent);
+                    }
+                    Assert.Contains("Kick user", kickButton.TextContent);
+                }
+            } 
+            else if (!isMod)
+            {
+                var kickButton = fragment.Find($"#{member.UserName}_kickButton");
+                Assert.Contains("Kick user", kickButton.InnerHtml);
+            }
+        }
+    }
+    
     [Fact]
     public async void GroupChatTests_CompChatSettings()
     {
@@ -243,12 +263,119 @@ public class GroupChatTests : AuthenticatedBlazoriseTestBase
         var (testUsers, testFriendships, testGroups, testInvites) 
             = await BuildTestCases();
         
+        // Arrange: User is Owner
+        var subjectUser = testUsers[0];
+        var subjectGroup = testGroups[0];
+        await SetUser(subjectUser);
+        UserService.Invalidate();
+
+        var component = Ctx.RenderComponent<CompChatSettings>(
+            parameters
+                => parameters.Add(p => p.ChatID, subjectGroup.ID)
+        );
+
+        var modList = component.Find("#modList");
+        var memberList = component.Find("#memberList");
+        
+        output.WriteLine(component.Markup);
+        // Assert: Expected number of entries
+        Assert.Equal(subjectGroup.Users.Count, memberList.Children.Length);
+        
+        // Assert: That expected elements are present
+        CompChatSettings_CheckMemberDisplay(subjectUser,subjectGroup,component);
+        
+        // Assert: Check observed the observed Chats for component
+        Assert.Equal(subjectGroup.ID, component.Instance.WatchedIDs[0]);
+        
+        // Arrange: User is Moderator
+        subjectUser = testUsers[1];
+        await SetUser(subjectUser);
+        UserService.Invalidate();
+
+        component.Dispose();
+        component = Ctx.RenderComponent<CompChatSettings>(
+            parameters
+                => parameters.Add(p => p.ChatID, subjectGroup.ID)
+        );
+        
+        // Arrange: User is Member
+        subjectUser = testUsers[2];
+        await SetUser(subjectUser);
+        UserService.Invalidate();
+
+        component.Dispose();
+        component = Ctx.RenderComponent<CompChatSettings>(
+            parameters
+                => parameters.Add(p => p.ChatID, subjectGroup.ID)
+        );
+    }
+
+    [Fact]
+    public async void CompChatSettings_ChangeAvatarTest()
+    {
+        // Arrange & Act: Set up test cases for Users and Group Chats
+        var (testUsers, testFriendships, testGroups, testInvites) 
+            = await BuildTestCases();
+        
+        // Arrange: User is Owner
         var subjectUser = testUsers[0];
         var subjectGroup = testGroups[0];
         await SetUser(subjectUser);
         UserService.Invalidate();
         
+        var component = Ctx.RenderComponent<CompChatSettings>(
+            parameters
+                => parameters.Add(p => p.ChatID, subjectGroup.ID)
+        );
         
+        // Arrange: No file passed
+        var spoofArgs = new InputFileChangeEventArgs(new List<IBrowserFile>());
+        // Act
+        await component.Instance.OnFileUpload(spoofArgs);
+        // Assert
+        var alertArgs = component.Instance.AlertArguments;
+        Assert.True(alertArgs.AlertVisible);
+        Assert.Equal("Error occured while uploading image.", alertArgs.AlertMessage);
+        Assert.Equal("No file was uploaded.",alertArgs.AlertDescription);
+        
+        // Arrange: Invalid file passed (not an image)
+        var fakeFile = new MockBrowserFile { ContentType = "file/csv" };
+        spoofArgs = new InputFileChangeEventArgs(new List<IBrowserFile> { fakeFile });
+        // Act
+        await component.Instance.OnFileUpload(spoofArgs);
+        // Assert
+        alertArgs = component.Instance.AlertArguments;
+        Assert.Equal("Uploaded file was not an image.",alertArgs.AlertDescription);
+        
+        // Arrange: Invalid file passed (image, but not png)
+        fakeFile = new MockBrowserFile { ContentType = "image/gif" };
+        spoofArgs = new InputFileChangeEventArgs(new List<IBrowserFile> { fakeFile });
+        // Act
+        await component.Instance.OnFileUpload(spoofArgs);
+        // Assert
+        alertArgs = component.Instance.AlertArguments;
+        Assert.Equal("Image must be a PNG or JPG (JPEG) file.",alertArgs.AlertDescription);
+        
+        // Arrange: Valid file passed, but image was empty
+        fakeFile = new MockBrowserFile { ContentType = "image/png", Bytes = Array.Empty<byte>()};
+        spoofArgs = new InputFileChangeEventArgs(new List<IBrowserFile> { fakeFile });
+        // Act
+        await component.Instance.OnFileUpload(spoofArgs);
+        // Assert
+        alertArgs = component.Instance.AlertArguments;
+        Assert.Equal("Unknown error occurred. Please try again.",alertArgs.AlertDescription);
+        
+        // Arrange: Valid file passed
+        fakeFile = new MockBrowserFile { ContentType = "image/png"};
+        spoofArgs = new InputFileChangeEventArgs(new List<IBrowserFile> { fakeFile });
+        // Act
+        await component.Instance.OnFileUpload(spoofArgs);
+        // Assert
+        alertArgs = component.Instance.AlertArguments;
+        Assert.Equal("Image was successfully uploaded.",alertArgs.AlertDescription);
+        
+        await component.Find("#settings-save").ClickAsync(new MouseEventArgs());
+        // TODO: This is really bad, but we can't exactly mock SQL calls in an easy way. Thus, the result is not checked.
     }
 
     [Fact]
@@ -258,9 +385,42 @@ public class GroupChatTests : AuthenticatedBlazoriseTestBase
         var (testUsers, testFriendships, testGroups, testInvites) 
             = await BuildTestCases();
         
+        // Arrange: User is Owner
         var subjectUser = testUsers[0];
         var subjectGroup = testGroups[0];
         await SetUser(subjectUser);
         UserService.Invalidate();
+
+        var component = Ctx.RenderComponent<CompChatSettings>(
+            parameters
+                => parameters.Add(p => p.ChatID, subjectGroup.ID)
+        );
+        
+        // Assert: Check observed the observed Chats for component
+        Assert.Equal(subjectGroup.ID, component.Instance.WatchedIDs[0]);
+        
+        // Arrange: User is Moderator
+        subjectUser = testUsers[1];
+        subjectGroup = testGroups[0];
+        await SetUser(subjectUser);
+        UserService.Invalidate();
+
+        component.Dispose();
+        component = Ctx.RenderComponent<CompChatSettings>(
+            parameters
+                => parameters.Add(p => p.ChatID, subjectGroup.ID)
+        );
+        
+        // Arrange: User is Member
+        subjectUser = testUsers[1];
+        subjectGroup = testGroups[0];
+        await SetUser(subjectUser);
+        UserService.Invalidate();
+
+        component.Dispose();
+        component = Ctx.RenderComponent<CompChatSettings>(
+            parameters
+                => parameters.Add(p => p.ChatID, subjectGroup.ID)
+        );
     }
 }
--- Tests/Provider/MockDatabaseProvider.cs
+++ Tests/Provider/MockDatabaseProvider.cs
@@ -2,6 +2,11 @@ using System.Linq.Expressions;
 using Bamboozlers.Classes.AppDbContext;
 using Bamboozlers.Classes.Func;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using MockQueryable.Moq;
 using Tests.Provider.MockAppDbContext;
@@ -15,17 +20,23 @@ public class MockDatabaseProvider
     public MockDatabaseProvider(TestContextBase ctx)
     {
         _mockDbContextFactory = new Mock<IDbContextFactory<AppDbContext>>();
-
+        
         var options = new DbContextOptions<AppDbContext>();
         var mockContext = new Mock<AppDbContext>(options);
+        var mockFacade = new Mock<DatabaseFacade>(mockContext.Object);
+        
         _mockAppDbContext = new MockAppDbContext.MockAppDbContext(mockContext);
+        
+        mockContext.Setup(x => x.Database).Returns(mockFacade.Object);
+        mockFacade.Setup(x => x.BeginTransactionAsync(default))
+            .ReturnsAsync(() => new Mock<IDbContextTransaction>().Object);
         
         _mockDbContextFactory.Setup(x => x.CreateDbContext()).Returns(_mockAppDbContext.MockDbContext.Object);
         _mockDbContextFactory.Setup(x => x.CreateDbContextAsync(default)).ReturnsAsync(_mockAppDbContext.MockDbContext.Object);
         
         ctx.Services.AddSingleton(GetDbContextFactory());
     }
-
+    
     public MockAppDbContext.MockAppDbContext GetMockAppDbContext()
     {
         return _mockAppDbContext;
