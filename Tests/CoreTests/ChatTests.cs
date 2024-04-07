acd71d4453decbe38c96a928ced57038a2c24a95
bhunt02 <bridgette.hunt02@outlook.com>
2024-04-07 01:18:36 -0700

    forgot an await

--- Tests/CoreTests/ChatTests.cs
+++ Tests/CoreTests/ChatTests.cs
@@ -28,7 +28,7 @@ public class ChatTests : AuthenticatedBlazoriseTestBase
 
         var component = Ctx.RenderComponent<CompChatView>(parameters => parameters
             .Add(p => p.ChatID, chat.ID));
-        component.Instance.UpdateMessages();
+        await component.Instance.UpdateMessages();
         
         // Assert
         Assert.NotNull(chat.Messages);
