19aa566e829c7b1da25e84537264fdbcb6f49f19
Bawnorton <bawnorton@me.com>
2024-04-10 20:27:32 -0700

    add message index

--- App/Classes/AppDbContext/MessageAttachment.cs
+++ App/Classes/AppDbContext/MessageAttachment.cs
@@ -1,13 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bamboozlers.Classes.AppDbContext;
 
+[PrimaryKey(nameof(ID), nameof(Index))]
 [SuppressMessage("ReSharper", "EntityFramework.ModelValidation.UnlimitedStringLength")]
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public class MessageAttachment
 {
-    [Key] public int ID { get; set; } 
+    public int ID { get; set; }
+    public int Index { get; set; }
     public string FileName { get; set; } = default!;
     public byte[] Data { get; set; } = default!;
 }
